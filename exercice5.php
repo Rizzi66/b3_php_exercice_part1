85582182ededaf39e169b5aa966a7728fb47f136
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-01-28 16:30:37 +0100

    exo 5 complete

--- exercice5.php
+++ exercice5.php
@@ -7,3 +7,31 @@
  * Utilisez ces méthodes pour effectuer des calculs mathématiques sur deux nombres.
  * En dessous de la classe, appeler chaque méthode
  */
+ 
+class Math {
+
+  static function add($a, $b): float {
+    return $a+$b;
+  }
+
+  static function subtract($a, $b): float {
+    return $a-$b;
+  }
+
+  static function multiply($a, $b): float {
+    return $a*$b;
+  }
+
+}
+
+$calcul1 = Math::add(2.45,3.12);
+$calcul2 = Math::subtract(10,8.4);
+$calcul3 = Math::multiply(5.3,2.4);
+
+echo $calcul1."<br/>";
+echo $calcul2."<br/>";
+echo $calcul3;
+
+
+
+

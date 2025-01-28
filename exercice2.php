c5ac5c78ac58dfacffd55cc66fdd04d1889dd625
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-01-28 15:55:49 +0100

    exercice 2 complete

--- exercice2.php
+++ exercice2.php
@@ -8,3 +8,79 @@
  * Implémentez une methode afficheInfos() pour afficher les informations de la personne.
  * Créer un nouvel objet et afficher les information
  */
+
+ 
+class Person {
+
+  public function __construct(protected string $first_name, 
+  protected string $last_name, 
+  protected int $age) {
+
+  }
+
+  public function afficherInfos(): string {
+    return $this->first_name." ".$this->last_name." - ".strval($this->age)." ans";
+  }
+
+
+
+
+  /**
+   * Get the value of first_name
+   */
+  public function getFirstName(): string
+  {
+    return $this->first_name;
+  }
+
+  /**
+   * Set the value of first_name
+   */
+  public function setFirstName(string $first_name): self
+  {
+    $this->first_name = $first_name;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of last_name
+   */
+  public function getLastName(): string
+  {
+    return $this->last_name;
+  }
+
+  /**
+   * Set the value of last_name
+   */
+  public function setLastName(string $last_name): self
+  {
+    $this->last_name = $last_name;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of age
+   */
+  public function getAge(): int
+  {
+    return $this->age;
+  }
+
+  /**
+   * Set the value of age
+   */
+  public function setAge(int $age): self
+  {
+    $this->age = $age;
+
+    return $this;
+  }
+}
+
+$person1 = new Person("Romain", "Segarizzi", 30);
+echo "Information de la personne : ".$person1->afficherInfos();
+
+

2864e420a367a37e80eb68c61a908854476e139b
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-01-28 16:20:43 +0100

    exercice 4 complete

--- exercice4.php
+++ exercice4.php
@@ -10,3 +10,30 @@
  * En dessous la classe instancier un objet et afficher les infos
 
  */
+
+ 
+class Article {
+
+  public function __construct(private string $title, 
+  private string $content) {
+
+  }
+
+  public function getSummary($maxLength): string {
+    if (strlen($this->content)>$maxLength) {
+      $newContent = substr($this->content, 0, $maxLength-3)."...";
+    } else {
+      $newContent = $this->content;
+    }
+    return $this->title."<br/>".$newContent;
+  }
+
+}
+
+$article1 = new Article("Veste", "Cette veste est en vente au prix de 99 €");
+echo $article1->getSummary(30)."<br/>";
+
+echo $article1->getSummary(60);
+
+
+

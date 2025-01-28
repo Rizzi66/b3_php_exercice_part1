9d3e41c61b4f8e61992a0a4ba1c4eb6f39f3107a
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-01-28 16:06:42 +0100

    exercice3 complete

--- exercice3.php
+++ exercice3.php
@@ -1,5 +1,28 @@
 <?php
 
+require_once "exercice2.php";
+
+class Employee extends Person {
+
+  public function __construct(string $first_name,  
+  string $last_name,  
+  string $age,
+  private float $salary, 
+  private string $position) 
+  {
+    parent::__construct($first_name, $last_name, $age);
+  }
+
+  public function afficherInfos(): string {
+    return $this->first_name." ".$this->last_name." - ".strval($this->age)." ans<br/>".
+    "Poste : ".$this->position."<br/>Salaire : ".$this->salary."€ par mois";
+  }
+
+}
+
+$employee1 = new Employee("Romain", "Segarizzi", 30, 2190.45, "Technicien vidéo");
+echo "<br/>Information de la personne : <br/>".$employee1->afficherInfos();
+
 /**
  * ÉNONCÉ :
  * Importer le fichier de l'exercice2 pour avoir accès à la classe Person

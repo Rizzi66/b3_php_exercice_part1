648ffb522c3e2c4af5153e643d4da5e4d2b4a6ff
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-01-28 16:59:15 +0100

    exercice8 not finish

--- exercice8.php
+++ exercice8.php
@@ -8,3 +8,42 @@
  * Implémentez les getters et setters pour manipuler le tableau de notes.
  * En dessous de la classe, créer un objet et appeler les méthodes
  */
+
+ require_once "exercice2.php";
+ 
+class Student extends Person {
+
+  public function __construct(string $first_name,  
+  string $last_name,  
+  string $age,
+  private array $grade)
+  {
+    parent::__construct($first_name, $last_name, $age);
+  }
+
+  public function getAverage(): float {
+    $moyenne = $this->grade / strlen($this->grade);
+    return $moyenne;
+  }
+
+
+  /**
+   * Get the value of grade
+   */
+  public function getGrade(): array
+  {
+    return $this->grade;
+  }
+
+  /**
+   * Set the value of grade
+   */
+  public function setGrade(array $grade): self
+  {
+    $this->grade = $grade;
+
+    return $this;
+  }
+}
+
+
